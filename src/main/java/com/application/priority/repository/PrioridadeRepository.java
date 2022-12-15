6aab0ae80f03dcbca2f0cafe1564e5d7387b4b93
Felipe Queiroz <felipe.de.a.queiroz@accenture.com>
2022-12-15 13:59:06 -0300

    Aplicação da anotação Repository para PrioridadeRepository

--- src/main/java/com/application/priority/repository/PrioridadeRepository.java
+++ src/main/java/com/application/priority/repository/PrioridadeRepository.java
@@ -2,6 +2,8 @@ package com.application.priority.repository;
 
 import com.application.priority.model.Prioridade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PrioridadeRepository extends JpaRepository<Prioridade, Integer> {
 }
